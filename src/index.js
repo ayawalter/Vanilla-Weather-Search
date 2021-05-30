ff0db3c16af7f2fa73f2f09ec4b1744066a3e7d2
ayawalter <84181746+ayawalter@users.noreply.github.com>
2021-05-30 23:25:47 +0200

    Added City search js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,22 @@ function formatDate(now) {
   return `${day} ${hour}:${minute}`;
 }
 
+function showWeather(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${response.data.name}`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let apikey = "85890638d00975101c866cb82a4d3716";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=${apikey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
 let lastUpdate = document.querySelector("#current-date-time");
 let now = new Date();
 lastUpdate.innerHTML = formatDate(now);
+
+let search = document.querySelector("form");
+search.addEventListener("submit", searchCity);
